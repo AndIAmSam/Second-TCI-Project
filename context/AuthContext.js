a6c34d7e0d90072b0317c143ce5800a71b488033
AndIAmSam <samflogar3@gmail.com>
2024-05-22 16:50:06 -0600

    Testing splash screen.

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,4 +1,3 @@
-// AuthContext.js
 import React, { createContext, useState, useContext } from "react";
 
 const AuthContext = createContext();
@@ -7,12 +6,10 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const signIn = () => {
-    // Aquí podrías realizar la lógica de autenticación simulada, por ejemplo, cambiar isAuthenticated a true
     setIsAuthenticated(true);
   };
 
   const signOut = () => {
-    // Aquí podrías realizar la lógica de cierre de sesión simulada, por ejemplo, cambiar isAuthenticated a false
     setIsAuthenticated(false);
   };
 
