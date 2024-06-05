efe9692c8706dd026f3ae7cccd783bcbcfb7be98
CarlosGD17 <carlosgd_17@outlook.com>
2024-06-05 12:10:15 -0600

    emil and img url

--- api/constants.js
+++ api/constants.js
@@ -1,2 +1,3 @@
 export const API_URL = 'http://20.49.18.239:1337';
-//export const API_URL = 'http://192.168.0.106:1337';
+export const EMAIL_API = 'http://20.49.18.239:3000';
+export const IMG_FOOTER = `<img src='https://cryptowallet.blob.core.windows.net/crypto-wallet/cryptologo.png' alt='Image description' style='width: auto; height: 250px;'>`;
